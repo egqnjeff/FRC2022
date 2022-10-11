936f0d9c6ec6cfb6033f52ee039567e31de1d59b
egqnjeff <egqnjeff@gmail.com>
2022-10-11 14:10:03 -0400

    Launcher converted

--- Robot2022/src/main/java/frc/robot/subsystems/Launcher.java
+++ Robot2022/src/main/java/frc/robot/subsystems/Launcher.java
@@ -4,15 +4,320 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ConSparkMax;
+import frc.robot.OI.ConShuffleboard;
 
 public class Launcher extends SubsystemBase {
-  /** Creates a new Launcher. */
-  public Launcher() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public static class ConLauncher {
+    public static int MOTOR_BERT_ID = 6;
+    public static int MOTOR_ERNIE_ID = 1;
+    // Starting point for Launcher soft limits
+    public static int BERT_FWD_LIMIT = 116;
+    public static int ERNIE_FWD_LIMIT = 115;
+    public static int BERT_REV_LIMIT = 0;
+    public static int ERNIE_REV_LIMIT = 0;  
+    public static int BERT_FAR_LIMIT = 176;
+    public static int ERNIE_FAR_LIMIT = 191;
+    public static double BERT_RAMP_RATE = .05;
+    public static double ERNIE_RAMP_RATE = .05;
+    public static double BERT_POWER = .96;
+    public static double ERNIE_POWER = .92;
+    public static double BERT_FAR_POWER = .77;
+    public static double ERNIE_FAR_POWER = .78;
+    public static int CURRENT_STALL_LIMIT = 80;
+    public static double DOUBLE_LAUNCH_PWR_SCALE_FACTOR = 1.05;
+  }
+  
+  ShuffleboardTab m_sbt_Launcher;
+  // Inputs from Dashboard
+  NetworkTableEntry m_nte_Bert_FwdLimit; // LAUNCHER forward may be MOTOR REVERSE!!!
+  NetworkTableEntry m_nte_Bert_RevLimit; // Ditto for the reverse
+  NetworkTableEntry m_nte_Ernie_FwdLimit; // LAUNCHER forward may be MOTOR REVERSE!!!
+  NetworkTableEntry m_nte_Ernie_RevLimit; // Ditto for the reverse
+
+  NetworkTableEntry m_nte_Bert_FarLimit; // Alternate
+  NetworkTableEntry m_nte_Ernie_FarLimit; // Alternate
+
+  NetworkTableEntry m_nte_Ernie_Power; // 0.0 to 1.0 (max)
+  NetworkTableEntry m_nte_Bert_Power; // Ditto
+
+  NetworkTableEntry m_nte_Ernie_FarPower; // Alternate
+  NetworkTableEntry m_nte_Bert_FarPower; // Alternate
+
+  NetworkTableEntry m_nte_Bert_Ramp_Rate;
+  NetworkTableEntry m_nte_Ernie_Ramp_Rate;
+
+  // Outputs to Dashboard
+  NetworkTableEntry m_nte_Ernie_Position; // Encoder feedback
+  NetworkTableEntry m_nte_Bert_Position; // Ditto
+  NetworkTableEntry m_nte_Bert_Voltage;
+  NetworkTableEntry m_nte_Ernie_Voltage;
+
+  // #ifdef LAUNCHER_VELOCITY_CONTROL
+    // Velocity Control- see
+    // https://github.com/REVrobotics/SPARK-MAX-Examples/tree/master/C%2B%2B/Velocity%20PID%20Control
+    NetworkTableEntry m_nte_Launcher_P__Gain;
+    NetworkTableEntry m_nte_Launcher_I_Gain;
+    NetworkTableEntry m_nte_Launcher_D_Gain;
+    NetworkTableEntry m_nte_Launcher_I_Zone;
+    NetworkTableEntry m_nte_Launcher_Feed_Forward;
+    NetworkTableEntry m_nte_Launcher_Max_Output;
+    NetworkTableEntry m_nte_Launcher_Min_Output;
+  // #endif
+  
+  // #ifdef ENABLE_LAUNCHER
+    // NEO motor
+    CANSparkMax m_launcherMotorBert = new CANSparkMax(ConLauncher.MOTOR_BERT_ID, CANSparkMax.MotorType.kBrushless);
+    CANSparkMax m_launcherMotorErnie = new CANSparkMax(ConLauncher.MOTOR_ERNIE_ID, CANSparkMax.MotorType.kBrushless);
+  
+    // built-in encoders
+    RelativeEncoder m_launcherEncoderBert = m_launcherMotorBert.getEncoder();
+    RelativeEncoder m_launcherEncoderErnie = m_launcherMotorErnie.getEncoder();
+  
+  // #ifdef LAUNCHER_VELOCITY_CONTROL
+    SparkMaxPIDController m_pidControllerBert = m_launcherMotorBert.getPIDController();
+    SparkMaxPIDController m_pidControllerErnie = m_launcherMotorErnie.getPIDController();
+  // #endif
+  
+  // #endif
+    boolean m_useClose = true;
+    
+    // Power Settings for each launcher
+    double m_ErnieFwdPower;
+    double m_BertFwdPower;
+  
+    double m_ErnieFwdLimit;
+    double m_BertFwdLimit;
+  
+    // Alternate settings
+    double m_ErnieFarPower;
+    double m_BertFarPower;
+  
+    double m_ErnieFarLimit;
+    double m_BertFarLimit;  
+  
+  public Launcher() {
+    // Initialize stuff here
+    // #ifdef ENABLE_LAUNCHER
+    // #ifdef LAUNCHER_VELOCITY_CONTROL
+    double kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
+    m_launcherMotorBert.restoreFactoryDefaults();
+    m_launcherMotorErnie.restoreFactoryDefaults();
+
+    // set PID coefficients
+    m_pidControllerBert.setP(kP);
+    m_pidControllerBert.setI(kI);
+    m_pidControllerBert.setD(kD);
+    m_pidControllerBert.setIZone(kIz);
+    m_pidControllerBert.setFF(kFF);
+    m_pidControllerBert.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_pidControllerErnie.setP(kP);
+    m_pidControllerErnie.setI(kI);
+    m_pidControllerErnie.setD(kD);
+    m_pidControllerErnie.setIZone(kIz);
+    m_pidControllerErnie.setFF(kFF);
+    m_pidControllerErnie.setOutputRange(kMinOutput, kMaxOutput);
+    // #endif
+
+    // All of our Encoders are based on a tick count of 42
+    m_launcherEncoderBert.setPositionConversionFactor(ConSparkMax.POSITION_CONVERSION_FACTOR); // Generally 42
+    m_launcherEncoderErnie.setPositionConversionFactor(ConSparkMax.POSITION_CONVERSION_FACTOR); // Generally 42
+
+    // I believe brake mode is the default, but...
+    m_launcherMotorBert.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    m_launcherMotorErnie.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    // No hardware limits on these controllers
+
+    m_launcherEncoderBert.setPosition(0.0);
+    m_launcherEncoderErnie.setPosition(0.0);
+    // Enable & Set Encoder Soft Limits...
+    m_launcherMotorBert.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    m_launcherMotorBert.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    m_launcherMotorErnie.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    m_launcherMotorErnie.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    // Forward Limits
+    m_launcherMotorBert.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) m_nte_Bert_FwdLimit.getDouble(ConLauncher.BERT_FWD_LIMIT));
+    m_launcherMotorBert.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) m_nte_Bert_RevLimit.getDouble(ConLauncher.BERT_REV_LIMIT));
+    m_launcherMotorErnie.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) m_nte_Ernie_FwdLimit.getDouble(ConLauncher.ERNIE_FWD_LIMIT));
+    m_launcherMotorErnie.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) m_nte_Ernie_RevLimit.getDouble(ConLauncher.ERNIE_REV_LIMIT));
+
+    // Naturally, Bert is Backwards...
+    m_launcherMotorBert.setInverted(true);
+    m_launcherMotorErnie.setInverted(false);
+
+    // Set Current limits (Stall, Free)
+    m_launcherMotorBert.setSmartCurrentLimit(ConLauncher.CURRENT_STALL_LIMIT, ConLauncher.CURRENT_STALL_LIMIT);
+    m_launcherMotorErnie.setSmartCurrentLimit(ConLauncher.CURRENT_STALL_LIMIT, ConLauncher.CURRENT_STALL_LIMIT);
+
+    // #endif            
+
+    m_sbt_Launcher = Shuffleboard.getTab(ConShuffleboard.LauncherTab);
+
+    // Robot outputs -> Shuffleboard
+    m_nte_Bert_Position = m_sbt_Launcher.addPersistent("Bert Pos", ConLauncher.BERT_REV_LIMIT)
+          .withSize(1,1)
+          .withPosition(0,0)
+          .getEntry();
+    m_nte_Ernie_Position = m_sbt_Launcher.addPersistent("Ernie Pos", ConLauncher.ERNIE_REV_LIMIT)
+          .withSize(1,1)
+          .withPosition(0,1)
+          .getEntry();
+
+    // Robot inputs <- Shuffleboard
+    m_nte_Bert_FwdLimit = m_sbt_Launcher.addPersistent("Bert Fwd Limit", ConLauncher.BERT_FWD_LIMIT)
+          .withSize(1,1)
+          .withPosition(2,0)
+          .getEntry();
+    m_nte_Ernie_FwdLimit = m_sbt_Launcher.addPersistent("Ernie Fwd Limit", ConLauncher.ERNIE_FWD_LIMIT)
+          .withSize(1,1)
+          .withPosition(2,1)
+          .getEntry();
+    m_nte_Bert_FarLimit = m_sbt_Launcher.addPersistent("Bert Far Limit", ConLauncher.BERT_FAR_LIMIT)
+          .withSize(1,1)
+          .withPosition(3,0)
+          .getEntry();
+    m_nte_Ernie_FarLimit = m_sbt_Launcher.addPersistent("Ernie Far Limit", ConLauncher.ERNIE_FAR_LIMIT)
+          .withSize(1,1)
+          .withPosition(3,1)
+          .getEntry();
+    m_nte_Bert_Power = m_sbt_Launcher.addPersistent("Bert Power", ConLauncher.BERT_POWER)
+          .withSize(1,1)
+          .withPosition(4,0)
+          .getEntry();
+    m_nte_Ernie_Power = m_sbt_Launcher.addPersistent("Ernie Power", ConLauncher.ERNIE_POWER)
+          .withSize(1,1)
+          .withPosition(4,1)
+          .getEntry();
+    m_nte_Bert_FarPower = m_sbt_Launcher.addPersistent("Bert Far Power", ConLauncher.BERT_FAR_POWER)
+          .withSize(1,1)
+          .withPosition(5,0)
+          .getEntry();
+    m_nte_Ernie_FarPower = m_sbt_Launcher.addPersistent("Ernie Far Power", ConLauncher.ERNIE_FAR_POWER)
+          .withSize(1,1)
+          .withPosition(5,1)
+          .getEntry();
+    m_nte_Bert_Voltage = m_sbt_Launcher.addPersistent("Bert Voltage", 0.0)
+          .withSize(2,1)
+          .withPosition(3,2)
+          .withWidget(BuiltInWidgets.kDial)
+          .getEntry();
+    m_nte_Ernie_Voltage = m_sbt_Launcher.addPersistent("Ernie Voltage", 0.0)
+          .withSize(2,1)
+          .withPosition(3,3)
+          .withWidget(BuiltInWidgets.kDial)
+          .getEntry();
+
+    // #ifdef LAUNCHER_VELOCITY_CONTROL
+    // display PID coefficients on Shuffleboard
+    m_nte_Launcher_P__Gain = m_sbt_Launcher.addPersistent("Launcher P Gain", kP)
+          .withSize(2,1)
+          .withPosition(6,0)
+          .getEntry();
+    m_nte_Launcher_I_Gain = m_sbt_Launcher.addPersistent("Launcher I Gain", kI)
+          .withSize(2,1)
+          .withPosition(6,1)
+          .getEntry();
+    m_nte_Launcher_D_Gain = m_sbt_Launcher.addPersistent("Launcher D Gain", kD)
+          .withSize(2,1)
+          .withPosition(6,2)
+          .getEntry();
+    m_nte_Launcher_I_Zone = m_sbt_Launcher.addPersistent("Launcher Iz Gain", kIz)
+          .withSize(2,1)
+          .withPosition(6,4)
+          .getEntry();
+    m_nte_Launcher_Min_Output = m_sbt_Launcher.addPersistent("Launcher Min Output", kMinOutput)
+          .withSize(2,1)
+          .withPosition(8,0)
+          .getEntry();
+    m_nte_Launcher_Max_Output = m_sbt_Launcher.addPersistent("Launcher Max Output", kMaxOutput)
+          .withSize(2,1)
+          .withPosition(8,1)
+          .getEntry();
+    m_nte_Launcher_Feed_Forward = m_sbt_Launcher.addPersistent("Launcher Feed Forward", kFF)
+          .withSize(2,1)
+          .withPosition(8,3)
+          .getEntry();
+    // #endif
+
+    m_ErnieFwdPower = ConLauncher.ERNIE_POWER;
+    m_BertFwdPower = ConLauncher.BERT_POWER;
+    m_ErnieFarPower = ConLauncher.ERNIE_FAR_POWER;
+    m_BertFarPower = ConLauncher.BERT_FAR_POWER;
+
+    // Ensure the launcher is in the retracted position
+    retract();
+  }
+
+  public void launch() {
+    launchBert(ConLauncher.DOUBLE_LAUNCH_PWR_SCALE_FACTOR);
+    launchErnie(ConLauncher.DOUBLE_LAUNCH_PWR_SCALE_FACTOR);
+  }
+
+  public void launchBert(double pwrScale) {
+    if(m_useClose) {
+      System.out.println("LaunchBert() Power " + m_BertFwdPower + "Limit " + m_BertFwdLimit);
+    } else {
+      System.out.println("LaunchBert() Power " + m_BertFarPower + "Limit " + m_BertFarLimit);
+    }
+    // Launch a ball
+    //#ifdef ENABLE_LAUNCHER
+    if (m_useClose) {
+      m_launcherMotorBert.set(m_BertFwdPower * pwrScale);
+    } else {
+      m_launcherMotorBert.set(m_BertFarPower * pwrScale);
+    }
+    System.out.println("Battery Voltage: " + RobotController.getBatteryVoltage());
+    //#endif
+  }
+
+  public void launchErnie(double pwrScale) {
+    if(m_useClose) {
+      System.out.println("LaunchErnie() Power " + m_ErnieFwdPower + "Limit " + m_ErnieFwdLimit);
+    } else {
+      System.out.println("LaunchErnie() Power " + m_ErnieFarPower + "Limit " + m_ErnieFarLimit);
+    } 
+    // Launch a ball
+    // #ifdef ENABLE_LAUNCHER
+    if (m_useClose) {
+      m_launcherMotorErnie.set(m_ErnieFwdPower * pwrScale);
+    } else {
+      m_launcherMotorErnie.set(m_ErnieFarPower * pwrScale);
+    }
+    System.out.println("Battery Voltage: " + RobotController.getBatteryVoltage());
+    // #endif
+  }
+
+  public void retract() {
+    // Bring both launchers back
+    retractBert();
+    retractErnie();
+  }
+
+  public void retractBert() {
+    System.out.println("Launcher::RetractBert() Executing...");  
+    // #ifdef ENABLE_LAUNCHER
+    m_launcherMotorBert.set(-.1);
+    // #endif
+  }
+
+  public void retractErnie() {
+    System.out.println("Launcher::RetractErnie() Executing...");  
+    // #ifdef ENABLE_LAUNCHER
+    m_launcherMotorErnie.set(-.1);
+    // #endif
   }
 }
 
