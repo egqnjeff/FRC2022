0e81713a646bba1203fce4c2f61265b031c951cf
egqnjeff <egqnjeff@gmail.com>
2022-10-14 12:25:30 -0400

    AutoDrive converted

--- Robot2022/src/main/java/frc/robot/commands/AutoDrive.java
+++ Robot2022/src/main/java/frc/robot/commands/AutoDrive.java
@@ -4,37 +4,133 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.DriveTrain.ConDriveTrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Launcher;
 
-public class AutoDrive extends CommandBase {
-  /** Creates a new AutoDrive. */
-  public AutoDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
+public class AutoDrive extends ParallelCommandGroup {
+  
+  private DriveTrain m_driveTrain;
+  private Launcher m_launcher;
+  private Intake m_intake;
+  
+  // The enum used as keys for selecting the command to run.
+  // enum CommandSelector { ONE, TWO, THREE };
+  
+  public AutoDrive(DriveTrain drivetrain, Launcher launcher, Intake intake) {
+    m_driveTrain = drivetrain;
+    m_launcher = launcher;
+    m_intake = intake;
+
+    // SHuffleboard parameters NOT refreshing this way. Moving them into the specific commands instead of
+    // passing them as arguments to the command seems to function as desired.
+    // units::time::second_t a = .5_s; // FIXME: Temporary pending proper type conversion double -> second_t
+    // double a = drivetrain->m_nte_a_DriveDelay.GetDouble(0.0); // Drive delay (seconds)
+    // double c = drivetrain->m_nte_c_DriveTurnAngle.GetDouble(0.0); // Turning Angle (degrees)
+    
+    // #if 1
+    // Should be set in AutonomousInit() as well as DisabledPeriodic()
+    //m_driveTrain->SetAutonomousParameters();
+    
+    double m_distance = m_driveTrain.m_autoDistance;
+    int m_mode = (int) (m_driveTrain.m_autoDriveMode+.5);
+    System.out.println("m_mode: " + m_mode + ", m_autoDriveMode: " + m_driveTrain.m_autoDriveMode);
+    System.out.println("Driving distance: " + m_distance);
+    System.out.println("Drive delay: " + m_driveTrain.m_autoDriveDelay);
+  
+    // Uncomment one of the following to force a specific Autonomous mode 
+    // m_mode = ConDriveTrain::AUTONOMOUS_MODE_2_BALL;
+    // m_mode = ConDriveTrain::AUTONOMOUS_MODE_LAUNCH_DELAY_MOVE;
+    
+    // Add your commands here, e.g.
+    // AddCommands(FooCommand(), BarCommand());
+    switch (m_mode) {
+      case ConDriveTrain.AUTONOMOUS_MODE_2_BALL:
+        System.out.println("Autonomous " + m_mode + " Ball Mode");
+        addCommands(
+          new SequentialCommandGroup(
+            new AutoDelay(0.5),
+            new InstantCommand(m_launcher::launch, m_launcher),
+            // new InstantCommand(m_launcher.launchErnie(), m_launcher),
+            new AutoDelay(0.5),
+            new InstantCommand(m_launcher::retract, m_launcher),
+            // new InstantCommand(m_launcher.retractErnie(), m_launcher),
+            new InstantCommand(intake::deploy, intake),
+            new AutoDriveDistance(m_driveTrain, m_distance),
+            new InstantCommand(intake::stow, intake),
+            new AutoDelay(0.5), 
+            new AutoDriveDistance(m_driveTrain, -m_distance),
+            // new InstantCommand(intake.deploy(), intake), // Only needed to avoid motor interference
+            new AutoDelay(0.5),
+            new InstantCommand(m_launcher::launch, m_launcher),
+            // new InstantCommand(m_launcher.launchErnie(), m_launcher),
+            new AutoDelay(0.5),
+            // new InstantCommand(intake.stow(), intake),
+            new InstantCommand(m_launcher::retract, m_launcher)
+            // new InstantCommand(m_launcher.retractErnie(), m_launcher),
+
+            // possible additional ball pickup?
+            // new AutoTurn(m_driveTrain, 45.0),
+            // new AutoDelay(0.5),
+            // new AutoDriveDistance(m_driveTrain, 84.0),
+          )
+        );
+      break;
+        
+      case ConDriveTrain.AUTONOMOUS_MODE_LAUNCH_DELAY_MOVE:
+        System.out.println("Autonomous Mode " + m_mode + ": Shoot-Delay-Move");
+        addCommands (
+          new SequentialCommandGroup (
+            new InstantCommand(m_launcher::launch, m_launcher),
+            // new InstantCommand(m_launcher::launchErnie, m_launcher),
+            new AutoDelay(m_driveTrain.m_autoDriveDelay),
+            new InstantCommand(m_launcher::retract, m_launcher),
+            // new InstantCommand(m_launcher::retractErnie, m_launcher),
+            new AutoDriveDistance(m_driveTrain, m_distance)
+          )
+        );
+        break;
+          
+      case ConDriveTrain.AUTONOMOUS_MODE_5_BALL:
+      break;
+          
+        // Default DO NOTHING
+      default:
+      break;
+          
+          // #if 0
+          // // Add autonomous drive & launcher commands
+          // AddCommands (
+          //   frc2::SequentialCommandGroup { AutoDelay(1.5_s), AutoDriveDistance(m_driveTrain), AutoTurn(m_driveTrain) },
+          //   frc2::SequentialCommandGroup{ AutoDelay(1.0_s), 
+          //     frc2::ParallelRaceGroup{ Deploy(intake), AutoDelay(1.0_s) }
+          //   }
+          //   //
+          //   //frc2::ParallelRaceGroup{ Launch(m_launcher), AutoDelay() },
+          //   //frc2::SequentialCommandGroup{ AutoDelay(1.0_s),
+          //     //                              frc2::ParallelRaceGroup{ Deploy(intake), AutoDelay(1.0_s) }
+          //     //                            }
+          //     //
+          //     );
+          //     #endif  // defined(ENABLE_LAUNCHER)
+              
+          //   } // switch(mode)
+          //   #endif // if 0
+            
+          // }
+          
+      }
+    }
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
-
-/** Original H
- // Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+        
+ /** Original H
+         // Copyright (c) FIRST and other WPILib contributors.
+         // Open Source Software; you can modify and/or share it under the terms of
+         // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
 
